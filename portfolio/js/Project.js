dd6b200390c6dc648d7907e255cb4742afa54203
iamndlovu <ndlovu.p@protonmail.com>
2021-01-18 11:08:40 +0200

    Project methods to build and show project preview

--- portfolio/js/Project.js
+++ portfolio/js/Project.js
@@ -1,43 +1,79 @@
-class Project
+export class Project
 {
-  constructor(title, description, hash, live, github) {
+  constructor(title, intro, description, hash, live, github) {
     this.title = title;
+    this.intro = intro;
     this.description = description;
     this.hash = hash;
     this.live = live;
     this.github = github;
   }
 
-  get title() {
+  get getTitle() {
     return this.title;
   }
 
-  get description() {
+  get getIntro() {
+    return this.intro;
+  }
+
+  get getDescription() {
     return this.description;
   }
 
-  get hash() {
-    return this.hash();
+  get getHash() {
+    return this.hash;
   }
 
   showCard(parent) {
     const card = this.buildCard();
     this.attach(parent, card);
-    //TODO: listen(readMoreClick, showMore)
   }
 
   showMore() {
     //TODO: buildMore()
     //TODO: attachToDOM()
     //TODO: handleClose()
+    alert('show more');
   }
   //TODO: buildCard()
   buildCard() {
+    const container = document.createElement('div');
+    const projectCard = document.createElement('div');
+    projectCard.className = 'project';
+    projectCard.name = this.getTitle;
 
+    const thumbnail = document.createElement('img');
+    thumbnail.className = 'thumbnail';
+    thumbnail.src = this.getHash;
+    thumbnail.alt = `${this.getTitle} app thumbnail`;
+
+    const projectPreview = document.createElement('div');
+    projectPreview.className = 'project-preview';
+
+    const projectTitle = document.createElement('h6');
+    projectTitle.className = 'project-title';
+    projectTitle.textContent = this.getTitle;
+
+    const projectIntro = document.createElement('p');
+    projectIntro.className = 'project-intro';
+    projectIntro.textContent = this.getIntro;
+
+    const moreButton = document.createElement('a');
+    moreButton.textContent = 'Read more'
+    moreButton.addEventListener('click', this.showMore);
+
+    projectPreview.appendChild(projectTitle);
+    projectPreview.appendChild(projectIntro);
+    projectPreview.appendChild(moreButton);
+    projectCard.appendChild(thumbnail);
+    projectCard.appendChild(projectPreview);
+    container.appendChild(projectCard);
+
+    return container;
   }
   //TODO: attach to container
   attach(parent, element) {
     parent.appendChild(element);
   }
-
-}
+};
