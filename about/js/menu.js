15fed32de247835fed56769e11cfbb889a743489
iamndlovu <pndlovu385@gmail.com>
2023-06-30 11:39:22 +0200

    dynamic footer year

--- about/js/menu.js
+++ about/js/menu.js
@@ -1,3 +1,5 @@
+document.getElementById('footer_year').textContent = new Date().getFullYear();
+
 //select DOM elements
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu-nav');
